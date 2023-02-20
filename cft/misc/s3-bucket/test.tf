c51da3e1811fdb0def1a9f55620e224fae92c96f
Tenable.cs <admin@accurics.com>
2023-02-20 09:45:43 +0000

    Auto Remediation By Tenable.cs

--- cft/misc/s3-bucket/test.tf
+++ cft/misc/s3-bucket/test.tf
@@ -1,10 +1,11 @@
 resource "aws_s3_bucket" "mybucket" {
   bucket = "mybucket"
-  acl = "public"
+  acl    = "public"
 
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
+        kms_master_key_id = "<kms_master_key_id>"
       }
     }
   }
